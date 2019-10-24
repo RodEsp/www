bf693f1c148f0f450526582f6774d61bf38e77b1
RodEsp <rod.esp.m.g@gmail.com>
2019-10-24 12:21:54 -0400

    Merge branch 'feature/express'

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,64 +1,48 @@
 var i, //Counter
-    linkIsOpen = false,
-    titleBar = document.getElementById("titleBar"),
-    nameDiv = document.getElementById("name"),
-    navLinks = document.getElementsByClassName("navLink"),
-    sections = document.getElementsByClassName("section");
+	linkIsOpen = false,
+	titleBar = document.getElementById('titleBar'),
+	nameDiv = document.getElementById('name'),
+	navLinks = document.getElementsByClassName('navLink'),
+	sections = document.getElementsByClassName('section');
 
 //Accepts an HTML element object and centers it in the browser window.
 var verticalCenterInBrowser = function (element) {
-    element.style.position = "relative";
-    element.style.top = Math.ceil((window.innerHeight / 2) - (element.clientHeight / 2)).toString() + "px";
+	element.style.position = 'relative';
+	element.style.top = Math.ceil((window.innerHeight / 2) - (element.clientHeight / 2)).toString() + 'px';
 };
 verticalCenterInBrowser(titleBar);
 
 window.onresize = function () {
-    verticalCenterInBrowser(titleBar);
+	verticalCenterInBrowser(titleBar);
 };
 
-var slide = function (element, heightCoord) {
-    var newHeight = 0;
-    if (element.getBoundingClientRect().top > heightCoord) {
-        newHeight = element.offsetTop - 1;
-    } else {
-        newHeight = element.offsetTop + 1;
-    }
-    element.style.top = newHeight.toString() + "px";
-
-    setTimeout(function () {
-        if (element.getBoundingClientRect().top != heightCoord) {
-            slide(element, heightCoord);
-        }
-    }, 0);
-};
-
-nameDiv.addEventListener("click", function () {
-    for (i = 0; i < sections.length; i++) {
-        sections[i].classList.remove("appearTransparent");
-    }
-    setTimeout(function () {
-        titleBar.classList.remove("slideUp");
-    }, 200);
-    linkIsOpen = false;
+nameDiv.addEventListener('click', function () {
+	for (i = 0; i < sections.length; i++) {
+		sections[i].classList.remove('appearTransparent');
+	}
+	setTimeout(function () {
+		titleBar.classList.remove('slideUp');
+	}, 200);
+	linkIsOpen = false;
 });
 
 for (i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", function () {
-        titleBar.classList.add("slideUp");
+	navLinks[i].addEventListener('click', function () {
+		titleBar.classList.add('slideUp');
 
-        for (var j = 0; j < sections.length; j++) {
-            sections[j].classList.remove("appearTransparent");
-        }
+		for (var j = 0; j < sections.length; j++) {
+			sections[j].classList.remove('appearTransparent');
+		}
 
-        var section = document.getElementById(this.innerText.toLowerCase());
-        if (!linkIsOpen) {
-            setTimeout(function () {
-                section.classList.add("appearTransparent");
-            }, 200);
-        } else {
-            section.classList.add("appearTransparent");
-        }
+		var section = document.getElementById(this.innerText.toLowerCase());
+		if (!linkIsOpen) {
+			setTimeout(function () {
+				section.classList.add('appearTransparent');
+			}, 200);
+		} else {
+			section.classList.add('appearTransparent');
+		}
 
-        linkIsOpen = true;
-    });
+		linkIsOpen = true;
+	});
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,6 @@
 var nodeExternals = require('webpack-node-externals');
 var path = require('path');
 
-// Add to dependencies
-// "@babel/core"
-// "@babel/node"
-// "@babel/preset-env"
-// "babel-loader"
-// module.rules = [{
-	// test: /\.m?js$/,
-	// exclude: /(node_modules)/,
-	// use: {
-	// 	loader: 'babel-loader',
-	// 	options: {
-	// 		presets: ['@babel/preset-env']
-	// 	}
-	// }
-// }];
-
 const nodeModules = nodeExternals();
 
 const clientConfig = {
@@ -24,6 +8,7 @@ const clientConfig = {
 		'./src/client/js/index.js',
 	],
 	target: 'web',
+	devtool: 'source-map',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'js/index.js'
@@ -32,15 +17,25 @@ const clientConfig = {
 };
 
 const serverConfig = {
-	entry: [
-		'./src/server/server.js',
-	],
+	entry: {
+		server: './src/server/server.js',
+	},
 	target: 'node',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'server.js'
 	},
-	externals: [nodeModules]
+	externals: [nodeModules], // Need this to avoid error when working with Express
+	module: {
+		rules: [{
+			// Transpiles ES6-8 into ES5
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader'
+			}
+		}]
+	}
 };
 
 module.exports = [clientConfig, serverConfig];
